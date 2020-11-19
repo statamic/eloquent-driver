55a5eb3e141c9f77a0157784879219025685bdc6
Jason Varga <jason@pixelfear.com>
2020-11-19 15:44:09 -0500

    Add phpunit and test

---
+++ tests/Entries/EntryModelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Entries;
+
+use PHPUnit\Framework\TestCase;
+use Statamic\Eloquent\Entries\EntryModel;
+
+class EntryModelTest extends TestCase
+{
+    /** @test */
+    public function it_gets_attributes_from_json_column()
+    {
+        $model = new EntryModel([
+            'slug' => 'the-slug',
+            'data' => [
+                'foo' => 'bar'
+            ]
+        ]);
+
+        $this->assertEquals('the-slug', $model->slug);
+        $this->assertEquals('bar', $model->foo);
+        $this->assertEquals(['foo' => 'bar'], $model->data);
+    }
+}
