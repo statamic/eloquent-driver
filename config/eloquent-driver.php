7b79b254f144bf7f5724c2eb31b5159db65f6047
Lukas <lukaskienzl@gmail.com>
2021-04-27 13:08:29 +0200

    fix typo

--- config/eloquent-driver.php
+++ config/eloquent-driver.php
@@ -4,7 +4,7 @@ return [
 
     'entries' => [
         'model' => \Statamic\Eloquent\Entries\EntryModel::class,
-        'base' => \Statamic\Eloquent\Entries\EntryModel::class,
+        'base' => \Statamic\Eloquent\Entries\Entry::class,
     ],
 
 ];
