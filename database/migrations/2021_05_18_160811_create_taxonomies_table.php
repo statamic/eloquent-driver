3d5e4f131884fbb960a7ccee59ac0afbd693e6f7
Oliver Kaufmann <oliver.kaufmann@novu.ch>
2021-05-19 16:11:59 +0200

    add migrations

---
+++ database/migrations/2021_05_18_160811_create_taxonomies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxonomiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxonomies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('handle');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxonomies');
+    }
+}
---
+++ database/migrations/2021_05_19_082853_create_terms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxonomy_terms', function (Blueprint $table) {
+            $table->id();
+            $table->string('site');
+            $table->string('slug');
+            $table->string('uri')->nullable();
+            $table->string('taxonomy');
+            $table->json('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxonomy_terms');
+    }
+}
