6a9d3220c663abf6b6640e308d970f83247e1b67
Jesse Leite <jesseleite@gmail.com>
2021-09-29 18:02:38 -0400

    Ensure top-level columns don't get saved to JSON data on update as well. References #29.

--- src/Entries/Entry.php
+++ src/Entries/Entry.php
@@ -40,7 +40,7 @@ class Entry extends FileEntry
             'uri' => $this->uri(),
             'date' => $this->hasDate() ? $this->date() : null,
             'collection' => $this->collectionHandle(),
-            'data' => $data,
+            'data' => $data->except(EntryQueryBuilder::COLUMNS),
             'published' => $this->published(),
             'status' => $this->status(),
         ]);
