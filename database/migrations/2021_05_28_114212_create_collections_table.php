c64e6cc2853d9e896a88082cee5d7e9f7fe5dc3a
Frederik Sauer <fritten.keez@gmail.com>
2022-07-13 10:56:12 +0200

    Removed unnecessary collection change migration for new installs

--- database/migrations/2021_05_28_114212_create_collections_table.php
+++ database/migrations/2021_05_28_114212_create_collections_table.php
@@ -17,22 +17,7 @@ class CreateCollectionsTable extends Migration
             $table->id();
             $table->string('handle');
             $table->string('title');
-            $table->json('routes')->nullable();
-            $table->boolean('dated')->default(false);
-            $table->string('past_date_behavior')->nullable();
-            $table->string('future_date_behavior')->nullable();
-            $table->boolean('default_publish_state')->default(true);
-            $table->boolean('ampable')->default(false);
-            $table->json('sites')->nullable();
-            $table->string('template')->nullable();
-            $table->string('layout')->nullable();
-            $table->string('sort_dir')->nullable();
-            $table->string('sort_field')->nullable();
-            $table->string('mount')->nullable();
-            $table->json('taxonomies')->nullable();
-            $table->boolean('revisions')->default(false);
-            $table->json('inject')->nullable();
-            $table->json('structure')->nullable();
+            $table->json('settings')->nullable();
             $table->timestamps();
         });
     }
