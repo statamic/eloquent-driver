8547ff03c0992b3b681de07dd44b16c0418e217d
Ryan Mitchell <ryan@thoughtcollective.com>
2022-10-27 08:18:39 +0100

    Add migration to tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -202,6 +202,9 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
         $migration = require __DIR__.'/../database/migrations/create_entries_table.php.stub';
         $migration->up();
+
+        $migration = require __DIR__.'/../database/migrations/2022_10_27_add_order_to_entries_table.php.stub';
+        $migration->up();
     }
 
     public function runMigrationsForUUIDEntries()
@@ -209,6 +212,9 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         $this->runBaseMigrations();
 
         $migration = require __DIR__.'/../database/migrations/create_entries_table_with_string_ids.php.stub';
+        $migration->up();
+
+        $migration = require __DIR__.'/../database/migrations/2022_10_27_add_order_to_entries_table.php.stub';
         $migration->up();
     }
 }
