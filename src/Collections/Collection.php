f1938478736ded5cce7f2f616a2cae1602da3bd0
Jason Varga <jason@pixelfear.com>
2022-11-02 14:18:12 -0400

    Use renamed version

--- src/Collections/Collection.php
+++ src/Collections/Collection.php
@@ -28,7 +28,7 @@ class Collection extends FileEntry
             ->searchIndex($model->settings['search_index'] ?? null)
             ->revisionsEnabled($model->settings['revisions'] ?? false)
             ->defaultPublishState($model->settings['default_status'] ?? true)
-            ->defaultLocalizationOrigin($model->settings['default_localization_origin'] ?? 'select')
+            ->originBehavior($model->settings['origin_behavior'] ?? 'select')
             ->structureContents($model->settings['structure'] ?? null)
             ->sortField($model->settings['sort_field'] ?? null)
             ->sortDirection($model->settings['sort_dir'] ?? null)
@@ -53,28 +53,28 @@ class Collection extends FileEntry
         return $class::firstOrNew(['handle' => $source->handle])->fill([
             'title'    => $source->title ?? $source->handle,
             'settings' => [
-                'routes'                        => $source->routes,
-                'slugs'                         => $source->requiresSlugs(),
-                'title_formats'                 => collect($source->titleFormats())->filter(),
-                'mount'                         => $source->mount,
-                'dated'                         => $source->dated,
-                'ampable'                       => $source->ampable,
-                'sites'                         => $source->sites,
-                'template'                      => $source->template,
-                'layout'                        => $source->layout,
-                'inject'                        => $source->cascade,
-                'search_index'                  => $source->searchIndex,
-                'revisions'                     => $source->revisionsEnabled(),
-                'default_status'                => $source->defaultPublishState,
-                'default_localization_origin'   => $source->defaultLocalizationOrigin(),
-                'structure'                     => $source->structureContents(),
-                'sort_dir'                      => $source->sortDirection(),
-                'sort_field'                    => $source->sortField(),
-                'taxonomies'                    => $source->taxonomies,
-                'propagate'                     => $source->propagate(),
-                'past_date_behavior'            => $source->pastDateBehavior(),
-                'future_date_behavior'          => $source->futureDateBehavior(),
-                'preview_targets'               => $source->previewTargets(),
+                'routes'               => $source->routes,
+                'slugs'                => $source->requiresSlugs(),
+                'title_formats'        => collect($source->titleFormats())->filter(),
+                'mount'                => $source->mount,
+                'dated'                => $source->dated,
+                'ampable'              => $source->ampable,
+                'sites'                => $source->sites,
+                'template'             => $source->template,
+                'layout'               => $source->layout,
+                'inject'               => $source->cascade,
+                'search_index'         => $source->searchIndex,
+                'revisions'            => $source->revisionsEnabled(),
+                'default_status'       => $source->defaultPublishState,
+                'structure'            => $source->structureContents(),
+                'sort_dir'             => $source->sortDirection(),
+                'sort_field'           => $source->sortField(),
+                'taxonomies'           => $source->taxonomies,
+                'propagate'            => $source->propagate(),
+                'past_date_behavior'   => $source->pastDateBehavior(),
+                'future_date_behavior' => $source->futureDateBehavior(),
+                'preview_targets'      => $source->previewTargets(),
+                'origin_behavior'      => $source->originBehavior(),
             ],
         ]);
     }
