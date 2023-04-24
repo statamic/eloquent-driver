f68204f0ad2c7496f8b1433df4c818d9d14c3ce6
Ryan Mitchell <ryan@thoughtcollective.com>
2023-04-24 21:14:26 +0100

    Verify collection is dated before setting entry date

--- src/Entries/Entry.php
+++ src/Entries/Entry.php
@@ -23,7 +23,7 @@ class Entry extends FileEntry
             ->published($model->published)
             ->model($model);
 
-        if ($model->date) {
+        if ($model->date && $entry->collection()->dated()) {
             $entry->date($model->date);
         }
 
