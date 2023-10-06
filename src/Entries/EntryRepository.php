386c06dea92658f2ed652e45602101a620647605
Danique Wijnalda <danique@van-ons.nl>
2023-10-06 14:04:49 +0200

    [Fix] Also md5 encoded uri in blink key

--- src/Entries/EntryRepository.php
+++ src/Entries/EntryRepository.php
@@ -35,7 +35,7 @@ class EntryRepository extends StacheRepository
 
     public function findByUri(string $uri, string $site = null): ?EntryContract
     {
-        $blinkKey = 'eloquent-entry-'.urlencode($uri).($site ? '-'.$site : '');
+        $blinkKey = 'eloquent-entry-'.md5(urlencode($uri)).($site ? '-'.$site : '');
         $item = Blink::once($blinkKey, function () use ($uri, $site) {
             return parent::findByUri($uri, $site);
         });
