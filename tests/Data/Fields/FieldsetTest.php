6f80b1f696693fe42157cd9fa3c375cf6b19110b
Ryan Mitchell <ryan@thoughtcollective.com>
2025-03-12 07:17:37 +0000

    Update test

--- tests/Data/Fields/FieldsetTest.php
+++ tests/Data/Fields/FieldsetTest.php
@@ -28,11 +28,14 @@ class FieldsetTest extends TestCase
     #[Test]
     public function it_handles_fieldsets_registered_by_addons()
     {
+        $this->assertCount(0, Fieldset::all());
+
         Fieldset::addNamespace(
             'my-addon',
             directory: __DIR__.'/../../__fixtures__/resources/fieldsets'
         );
 
-        $this->assertCount(1, Fieldset::all()); // we check theres no new  database entries, ie its been handled by files
+        $this->assertCount(1, Fieldset::all());
+        $this->assertSame('my-addon::seo', Fieldset::all()->first()->handle());
     }
 }
