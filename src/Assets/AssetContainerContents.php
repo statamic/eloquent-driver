b4460447d29159dea18e97f264e3b9bd64c55adf
Ryan Mitchell <ryan@thoughtcollective.com>
2025-06-12 08:33:11 +0100

    Ensure empty directories are included in folder listing (#454)

--- src/Assets/AssetContainerContents.php
+++ src/Assets/AssetContainerContents.php
@@ -40,10 +40,20 @@ class AssetContainerContents extends CoreAssetContainerContents
         }
 
         $this->folders = Cache::remember($this->key(), $this->ttl(), function () {
-            return $this->query()->select(['folder'])
-                ->distinct()
-                ->get()
-                ->map(fn ($model) => ['path' => $model->folder, 'type' => 'dir']);
+            return
+                collect(
+                    $this->query()->select(['folder'])
+                        ->distinct()
+                        ->get()
+                        ->map(fn ($model) => ['path' => $model->folder, 'type' => 'dir'])
+                )
+                    ->merge(
+                        collect($this->container->disk()->getFolders('/', true)
+                            ->filter(fn ($folder) => ! Str::startsWith($folder, '.'))
+                        )
+                            ->map(fn ($folder) => ['path' => $folder, 'type' => 'dir'])
+                    )
+                    ->unique();
         });
 
         return $this->folders;
