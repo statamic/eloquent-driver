bd89d3e23ce742f6aa260e085246a9e66b64768e
Ryan Mitchell <ryan@thoughtcollective.com>
2025-07-04 18:44:40 +0100

    [5.x] Allow globals and variables to be exported seperately (#431)
    * Allow globals and variables to be exported seperately
    
    * The origin is now defined on the global set
    
    * Change approach to export command
    
    * Update and add some tests thanks to Claude
    
    ---------
    
    Co-authored-by: Duncan McClean <duncan@duncanmcclean.com>

--- src/Commands/ExportGlobals.php
+++ src/Commands/ExportGlobals.php
@@ -2,7 +2,6 @@
 
 namespace Statamic\Eloquent\Commands;
 
-use Closure;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Facade;
 use Statamic\Console\RunsInPlease;
@@ -28,7 +27,10 @@ class ExportGlobals extends Command
      *
      * @var string
      */
-    protected $signature = 'statamic:eloquent:export-globals';
+    protected $signature = 'statamic:eloquent:export-globals
+        {--force : Force the export to run, with all prompts answered "yes"}
+        {--only-globals : Only export global sets}
+        {--only-variables : Only export global variables}';
 
     /**
      * The console command description.
@@ -44,47 +46,75 @@ class ExportGlobals extends Command
      */
     public function handle()
     {
-        $this->usingDefaultRepositories(function () {
-            $this->exportGlobals();
-        });
+        // ensure we are using stache globals, no matter what our config is
+        Facade::clearResolvedInstance(GlobalRepositoryContract::class);
+        Statamic::repository(GlobalRepositoryContract::class, GlobalRepository::class);
+        app()->bind(GlobalSetContract::class, GlobalSet::class);
+
+        // ensure we are using stache variables, no matter what our config is
+        Facade::clearResolvedInstance(GlobalVariablesRepositoryContract::class);
+        Statamic::repository(GlobalVariablesRepositoryContract::class, GlobalVariablesRepository::class);
+        app()->bind(VariablesContract::class, Variables::class);
+
+        $this->exportGlobals();
+        $this->exportGlobalVariables();
 
         return 0;
     }
 
-    private function usingDefaultRepositories(Closure $callback)
-    {
-        Facade::clearResolvedInstance(GlobalRepositoryContract::class);
-        Facade::clearResolvedInstance(GlobalVariablesRepositoryContract::class);
-
-        Statamic::repository(GlobalRepositoryContract::class, GlobalRepository::class);
-        Statamic::repository(GlobalVariablesRepositoryContract::class, GlobalVariablesRepository::class);
-
-        app()->bind(GlobalSetContract::class, GlobalSet::class);
-        app()->bind(VariablesContract::class, Variables::class);
-
-        $callback();
-    }
-
     private function exportGlobals()
     {
+        if (! $this->shouldExportGlobals()) {
+            return;
+        }
+
         $sets = GlobalSetModel::all();
-        $variables = VariablesModel::all();
 
-        $this->withProgressBar($sets, function ($model) use ($variables) {
-            $global = GlobalSetFacade::make()
+        $this->withProgressBar($sets, function ($model) {
+            GlobalSetFacade::make()
                 ->handle($model->handle)
-                ->title($model->title);
-
-            foreach ($variables->where('handle', $model->handle) as $localization) {
-                $global->makeLocalization($localization->locale)
-                    ->data($localization->data)
-                    ->origin($localization->origin ?? null);
-            }
-
-            $global->save();
+                ->title($model->title)
+                ->sites($model->sites)
+                ->save();
         });
 
         $this->newLine();
         $this->info('Globals exported');
+    }
+
+    private function exportGlobalVariables()
+    {
+        if (! $this->shouldExportVariables()) {
+            return;
+        }
+
+        $variables = VariablesModel::all();
+
+        $this->withProgressBar($variables, function ($model) {
+            if (! $global = GlobalSetFacade::find($model->handle)) {
+                return;
+            }
+
+            $globalVariable = $global->makeLocalization($model->locale);
+            $globalVariable->data($model->data);
+            $globalVariable->save();
+        });
+
+        $this->newLine();
+        $this->info('Global variables exported');
+    }
+
+    private function shouldExportGlobals(): bool
+    {
+        return $this->option('only-globals')
+            || ! $this->option('only-variables')
+            && ($this->option('force') || $this->confirm('Do you want to export global sets?'));
+    }
+
+    private function shouldExportVariables(): bool
+    {
+        return $this->option('only-variables')
+            || ! $this->option('only-globals')
+            && ($this->option('force') || $this->confirm('Do you want to export global variables?'));
     }
 }
