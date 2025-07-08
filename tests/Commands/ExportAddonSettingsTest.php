015f0f3f2f95df168803cbe636c78070e04a12b5
Duncan McClean <duncan@duncanmcclean.com>
2025-07-08 11:33:35 +0100

    Fix export command test

--- tests/Commands/ExportAddonSettingsTest.php
+++ tests/Commands/ExportAddonSettingsTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Commands;
 
+use Foo\Bar\TestAddonServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\File;
 use PHPUnit\Framework\Attributes\Test;
 use Statamic\Eloquent\AddonSettings\AddonSettingsModel;
+use Statamic\Extend\Addon;
+use Statamic\Facades;
 use Tests\TestCase;
 
 class ExportAddonSettingsTest extends TestCase
@@ -15,6 +18,13 @@ class ExportAddonSettingsTest extends TestCase
     #[Test]
     public function it_exports_addon_settings()
     {
+        $seoPro = $this->makeFromPackage(['id' => 'statamic/seo-pro', 'slug' => 'seo-pro']);
+        $importer = $this->makeFromPackage(['id' => 'statamic/importer', 'slug' => 'importer']);
+
+        Facades\Addon::shouldReceive('all')->andReturn(collect([$seoPro, $importer]));
+        Facades\Addon::shouldReceive('get')->with('statamic/seo-pro')->andReturn($seoPro);
+        Facades\Addon::shouldReceive('get')->with('statamic/importer')->andReturn($importer);
+
         AddonSettingsModel::create(['addon' => 'statamic/seo-pro', 'settings' => ['title' => 'SEO Title', 'description' => 'SEO Description']]);
         AddonSettingsModel::create(['addon' => 'statamic/importer', 'settings' => ['chunk_size' => 100]]);
 
@@ -22,19 +32,36 @@ class ExportAddonSettingsTest extends TestCase
             ->expectsOutputToContain('Addon settings exported')
             ->assertExitCode(0);
 
-        $this->assertFileExists(resource_path('addons/statamic/seo-pro.yaml'));
+        $this->assertFileExists(resource_path('addons/seo-pro.yaml'));
         $this->assertEquals(<<<'YAML'
 title: 'SEO Title'
 description: 'SEO Description'
 
 YAML
-            , File::get(resource_path('addons/statamic/seo-pro.yaml')));
+            , File::get(resource_path('addons/seo-pro.yaml')));
 
-        $this->assertFileExists(resource_path('addons/statamic/importer.yaml'));
+        $this->assertFileExists(resource_path('addons/importer.yaml'));
         $this->assertEquals(<<<'YAML'
 chunk_size: 100
 
 YAML
-            , File::get(resource_path('addons/statamic/importer.yaml')));
+            , File::get(resource_path('addons/importer.yaml')));
+    }
+
+    private function makeFromPackage($attributes = [])
+    {
+        return Addon::makeFromPackage(array_merge([
+            'id' => 'vendor/test-addon',
+            'name' => 'Test Addon',
+            'description' => 'Test description',
+            'namespace' => 'Vendor\\TestAddon',
+            'provider' => TestAddonServiceProvider::class,
+            'autoload' => '',
+            'url' => 'http://test-url.com',
+            'developer' => 'Test Developer LLC',
+            'developerUrl' => 'http://test-developer.com',
+            'version' => '1.0',
+            'editions' => ['foo', 'bar'],
+        ], $attributes));
     }
 }
